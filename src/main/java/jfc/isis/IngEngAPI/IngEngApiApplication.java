c97371e027b0d4bcf762135326ff5d97674346dc
Louna_Bashti <bashlodu59@orange.fr>
2023-03-10 16:03:12 +0100

    Initial commit (base appli)

---
+++ src/main/java/jfc/isis/IngEngAPI/IngEngApiApplication.java
@@ -0,0 +1,13 @@
+package jfc.isis.IngEngAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngEngApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngEngApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jfc/isis/IngEngAPI/IngEngApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jfc.isis.IngEngAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngEngApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
